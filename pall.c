f9dc88c8ce97c759dac23584fc5c2e1d4ff05043
Cloudtastrophe <ifiwil@yahoo.com>
2024-01-18 18:52:40 -0800

    commit

--- pall.c
+++ pall.c
@@ -11,16 +11,16 @@
  */
 void f_pall(stack_t **head, unsigned int counter)
 {
-    stack_t *h;
-    (void)counter;
+	stack_t *h;
+	(void)counter;
 
-    h = *head;
-    if (h == NULL)
-        return;
+	h = *head;
+	if (h == NULL)
+		return;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-    }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
--- pint.c
+++ pint.c
@@ -1,19 +1,27 @@
 #include "monty.h"
+
 /**
- * f_pint - prints from the top
+ * f_pint - prints the value at the top of the stack
  * @head: stack head
  * @counter: line_number
- * Return: no return
-*/
+ *
+ * Description: Prints the value at the top of the stack, followed by a new line.
+ * If the stack is empty, it prints an error message and exits with EXIT_FAILURE.
+ *
+ * Return: No return value
+ */
 void f_pint(stack_t **head, unsigned int counter)
 {
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*head)->n);
+    stack_t *top = *head;
+
+    if (top == NULL)
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", top->n);
 }
